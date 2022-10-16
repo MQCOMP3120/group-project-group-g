278be1a16de9d88df2d01e83c91b2e8e67bf8047
Braydon <Braydon.chi22@gmail.com>
2022-10-16 13:57:46 +1100

    dropdown search bar

---
+++ src/components/DropDownSearchBar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+import { BiSearch } from "react-icons/bi";
+import { Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { closeSearch } from "../features/navbar/searchSlice";
+
+export default function DropDownSearchBar({ productOptions, onInputChange }) {
+  const dispatch = useDispatch();
+  return (
+    <Form className="search-bar-dropdown d-flex">
+      <Form.Group>
+        <input
+          autoFocus
+          type="text"
+          className="form-control"
+          placeholder="Search product"
+          onChange={onInputChange}
+          autofocus
+        />
+
+        <ul className="list-group">
+          {productOptions.map((productOption) => {
+            return (
+              <Button
+                key={productOption.id}
+                type="button"
+                className="list-group-item list-group-item-action"
+                as={Link}
+                to={`/products/${productOption.id}`}
+                onClick={() => dispatch(closeSearch())}
+              >
+                {productOption.name}
+              </Button>
+            );
+          })}
+        </ul>
+      </Form.Group>
+    </Form>
+  );
+}
