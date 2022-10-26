6e432d162623025ac3662da7f6ea3686b1ca2bcf
mitchellhrnjak <mitchellhrnjak@gmail.com>
2022-10-26 21:45:04 +1100

    new styles

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -36,9 +36,9 @@ export default function Login() {
   return (
     <Wrapper className="center-items section-center">
       <Form noValidate validated={isSignIn} onSubmit={handleSubmit}>
-        <Form.Group className="mb-2 d-flex justify-content-between">
-          <h2 className="current-page p-3"> Login </h2>
-          <Link to="/register" className="p-3">
+        <Form.Group className="d-flex justify-content-between">
+          <h2 className="welcome-text"> Login </h2>
+          <Link to="/register">
             <p className="register-btn">
               Register
               <AiOutlineRight />
@@ -46,6 +46,8 @@ export default function Login() {
           </Link>
         </Form.Group>
         <Form.Group className="mb-3">
+          <p>Login to continue</p>
+
           <Form.FloatingLabel controlId="usernameInput" label="Username">
             <Form.Control
               type="username"
@@ -54,7 +56,7 @@ export default function Login() {
             />
           </Form.FloatingLabel>
         </Form.Group>
-        <Form.Group className="mb-5">
+        <Form.Group className="mb-4">
           <Form.FloatingLabel controlId="passwordInput" label="Password">
             <Form.Control
               type="password"
@@ -63,8 +65,9 @@ export default function Login() {
             />
           </Form.FloatingLabel>
         </Form.Group>
-        <Form.Group className="d-grid mb-3">
-          <button type="submit" className="btn btn-primary" id="login-btn">
+        <Form.Group>
+          <p className="register-btn">Forgot your password?</p>
+          <button type="submit" className="login-button">
             Submit
           </button>
         </Form.Group>
@@ -94,10 +97,35 @@ const Wrapper = styled.section`
     height: 60vh;
     min-width: 30vw;
     padding: 2rem;
-    border-radius: 15px;
-    border: 1px solid #787672;
+  }
+  .welcome-text {
+    font-family: "Poppins";
+    font-style: normal;
+    font-weight: 900;
+    font-size: 30px;
   }
 
+  .login-button {
+    padding: 14px 28px;
+    gap: 10px;
+    width: 456px;
+    height: 44px;
+    background: #444444;
+    border-radius: 6px;
+
+    font-family: "Poppins";
+    font-style: normal;
+    font-size: 14px;
+    line-height: 16px;
+    color: #ffffff;
+  }
+  .register-btn {
+    font-family: "Poppins";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 21px;
+  }
   .register-btn:hover {
     color: #0275d8;
   }
@@ -106,9 +134,8 @@ const Wrapper = styled.section`
     text-align: center;
     border-bottom: 2px solid #787672;
     line-height: 0.1em;
-    margin: 10px 0 20px;
+    margin: 20px 0 20px;
   }
-
   .break-line span {
     background: #fff;
     padding: 0 10px;
