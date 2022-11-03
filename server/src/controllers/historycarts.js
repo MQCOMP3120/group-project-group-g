33264a4c48b582f62a1fc55516d6f1c7f6fb4b6b
ben <fschenbin@gmail.com>
2022-11-03 17:03:29 +1100

    add more payment option

--- server/src/controllers/historycarts.js
+++ server/src/controllers/historycarts.js
@@ -145,18 +145,23 @@ const stripeCheckout = async (req, res) => {
     try {
         // Create a checkout session with Stripe
         const session = await stripe.checkout.sessions.create({
-          payment_method_types: ["card"],
+          payment_method_types: ["card","alipay","afterpay_clearpay"],
           // For each item use the id to get it's information
           // Take that information and convert it to Stripe's format
           line_items: req.body.items.map(({ id, title, price, quantity }) => {
             ///const storeItem = storeItems.get(id)
             return {
               price_data: {
-                currency: "usd",
+                currency: "aud",
                 product_data: {
                   name: title,
                 },
                 unit_amount: price*100,
+              },
+              adjustable_quantity: {
+                enabled: true,
+                minimum: 1,
+                maximum: 10
               },
               quantity: quantity,
             }
@@ -165,6 +170,10 @@ const stripeCheckout = async (req, res) => {
           // Set a success and cancel URL we will send customers to
           // These must be full URLs
           // In the next section we will setup CLIENT_URL
+          shipping_address_collection: {
+            // Specify which shipping countries Checkout should provide as options for shipping locations
+            allowed_countries: ['AU', 'GB', 'NZ', 'US'],
+          },
           success_url: `${process.env.CORS_CLIENT_DOMAIN}/success.html`,
           cancel_url: `${process.env.CORS_CLIENT_DOMAIN}/cancel.html`,
         })
