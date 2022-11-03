02cd4d89a5f8216543716d36575cd8ce02274736
ben <fschenbin@gmail.com>
2022-11-03 20:11:31 +1100

    stripe add success page

---
+++ src/pages/success.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+function Success() {
+    console.log("Success");
+    return (
+      <main>
+        
+      </main>
+    );
+  }
+
+  export default Success
+  
+
