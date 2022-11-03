5858c32f28185e2cc557b423ce0e1eea03a6715a
ben <fschenbin@gmail.com>
2022-11-03 15:07:10 +1100

    add stripe back-end

--- server/src/controllers/historycarts.js
+++ server/src/controllers/historycarts.js
@@ -146,6 +146,49 @@ const deleteHistoryCarts = async (request, response) => {
   response.status(200).json({ status: "OK" });
 };
 
+
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
+
+const stripeCheckout = async (req, res) => {
+  // console.log("stripeCheckout")
+  // response.status(200).json({url: "hi"})
+
+  try {
+      // Create a checkout session with Stripe
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        // For each item use the id to get it's information
+        // Take that information and convert it to Stripe's format
+        line_items: req.body.items.map(({ id, title, price, quantity }) => {
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: title,
+              },
+              unit_amount: price*100,
+            },
+            quantity: quantity,
+          }
+        }),
+        mode: "payment",
+        // Set a success and cancel URL we will send customers to
+        // These must be full URLs
+        // In the next section we will setup CLIENT_URL
+        success_url: `${process.env.CORS_CLIENT_DOMAIN}/success.html`,
+        cancel_url: `${process.env.CORS_CLIENT_DOMAIN}/cancel.html`,
+      })
+      //res.redirect(303, session.url);
+      res.json({ url: session.url })
+    } catch (e) {
+      // If there is an error send it to the client
+      res.status(500).json({ error: e.message })
+    }
+}
+
+
+
+
 module.exports = {
   createHistoryCart,
   getHistoryCarts,
@@ -153,4 +196,5 @@ module.exports = {
   getHistoryUserCarts,
   deleteHistoryCart,
   deleteHistoryCarts,
+  stripeCheckout,
 };
