dad0a071a078f4a87ba952dfa9e1d097b5903ffe
Braydon <Braydon.chi22@gmail.com>
2022-11-03 18:26:47 +1100

    stripe test2

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,19 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
   getCart,
   delCart,
   delCartProduct,
-  setCartSummary,
   increaseProductQuantity,
   decreaseProductQuantity,
   putCart,
+  payCart,
+  postCartHistory,
 } from "../features/cart/cartSlice";
 import { useNavigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
 import styled from "styled-components";
 import { toast } from "react-toastify";
+import { serverUrl } from "../util/api";
 
 export default function Cart() {
   const dispatch = useDispatch();
@@ -22,7 +24,10 @@ export default function Cart() {
   const { userCart, isLoading, cartProducts } = useSelector(
     (state) => state.cart
   );
+
   const { allProducts } = useSelector((state) => state.filter);
+  let stripeCartSummary = [];
+
   useEffect(() => {
     if (!isSignIn) {
       navigate("/login");
@@ -34,6 +39,7 @@ export default function Cart() {
   if (isLoading) {
     return <h1> Loading ... </h1>;
   }
+  console.log(userCart);
 
   const handleRemoveCartItem = (id) => {
     const notifyProductRemove = () =>
@@ -70,24 +76,24 @@ export default function Cart() {
     return allProducts.filter((product) => product.id === id);
   };
 
-  const calculateSubtotal = () => {
+  const subtotalAndSummary = () => {
     let total = 0;
-    cartProducts.forEach((product) => {
-      const { price } = getProduct(product.productId)[0];
+    stripeCartSummary = [];
+    cartProducts.forEach((product, idx) => {
+      const { price, title } = getProduct(product.productId)[0];
       total += price * product.quantity;
+
+      //cart summary send to stripe
+      stripeCartSummary = stripeCartSummary.concat({
+        id: idx,
+        title: title,
+        price: price,
+        quantity: product.quantity,
+      });
     });
 
     // dispatch(setSubtotal(total))
     return total;
-  };
-
-  const handleCheckout = () => {
-    const cartSummary = {
-      cartProducts: cartProducts,
-      subtotal: calculateSubtotal(),
-    };
-    dispatch(setCartSummary(cartSummary));
-    navigate("/payment");
   };
 
   const emptyCart = <p> Your cart is currently empty </p>;
@@ -139,13 +145,47 @@ export default function Cart() {
           );
         });
 
+  // stripe
+  const stripePay = (e) => {
+    e.preventDefault();
+    dispatch(payCart());
+    dispatch(postCartHistory());
+    dispatch(delCart());
+
+    fetch(`${serverUrl}/create-checkout-session`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Send along all the information about the items
+      body: JSON.stringify({
+        items: [...stripeCartSummary],
+      }),
+    })
+      .then((res) => {
+        if (res.ok) return res.json();
+        // If there is an error then make sure we catch that
+        return res.json().then((e) => Promise.reject(e));
+      })
+      .then(({ url }) => {
+        // On success redirect the customer to the returned URL
+
+        window.location = url;
+      })
+      .catch((e) => {
+        console.error(e.error);
+      });
+
+    // console.log(cartSummary);
+  };
+
   return (
     <Wrapper className="section-center h-100">
       <h3 className="my-5"> Cart </h3>
       {productElem}
       {userCart[0] && cartProducts.length >= 1 && (
         <>
-          <h2 className="my-2"> Subtotal: ${calculateSubtotal()}</h2>
+          <h2 className="my-2"> Subtotal: ${subtotalAndSummary()}</h2>
           <div className="buttons">
             <Button
               variant="outline-danger"
@@ -159,7 +199,7 @@ export default function Cart() {
             <Button
               variant="primary"
               className="checkout-btn"
-              onClick={() => handleCheckout()}
+              onClick={stripePay}
             >
               {" "}
               Check Out{" "}
--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,136 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import styled from "styled-components";
 import { payCart, delCart, postCartHistory } from "../features/cart/cartSlice";
-import PaymentForm from "../components/PaymentForm";
-import { toast } from "react-toastify";
-import { serverUrl } from "../util/api";
-import { Button } from "react-bootstrap";
 
-export default function Payment() {
+export default function SuccessFulPayment() {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const { cartSummary } = useSelector((state) => state.cart);
-  const { allProducts } = useSelector((state) => state.filter);
 
-  const getProduct = (id) => {
-    return allProducts.filter((product) => product.id === id);
-  };
+  // useEffect(() => {
+  //   // console.log(cartSummary);
+  //   // dispatch(payCart());
+  //   // dispatch(postCartHistory());
+  //   // dispatch(delCart());
+  // }, []);
 
-  const handlePayment = () => {
-    dispatch(payCart());
-    dispatch(postCartHistory());
-    dispatch(delCart());
-    navigate("/products");
-    // const notify = () =>
-    //   toast.success("Payment successful!", {
-    //     position: "top-right",
-    //     autoClose: 5000,
-    //     hideProgressBar: false,
-    //     closeOnClick: true,
-    //     pauseOnHover: true,
-    //     draggable: true,
-    //     progress: undefined,
-    //     theme: "light",
-    //   });
-
-    // notify();
-  };
-
-  const stripePay = (e) => {
-    e.preventDefault();
-    fetch(`${serverUrl}/create-checkout-session`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // Send along all the information about the items
-      body: JSON.stringify({
-        items: [
-          {
-            id: 1,
-            title: "iphone4",
-            price: 100,
-            quantity: 2,
-          },
-          {
-            id: 1,
-            title: "iphone5",
-            price: 100,
-            quantity: 2,
-          },
-        ],
-      }),
-    })
-      .then((res) => {
-        if (res.ok) return res.json();
-        // If there is an error then make sure we catch that
-        return res.json().then((e) => Promise.reject(e));
-      })
-      .then(({ url }) => {
-        // On success redirect the customer to the returned URL
-        window.location = url;
-      })
-      .catch((e) => {
-        console.error(e.error);
-      });
-  };
-
-  const summary = cartSummary.cartProducts ? (
-    cartSummary.cartProducts.map((product) => {
-      const { price, title, image } = getProduct(product.productId)[0];
-
-      return (
-        <div key={product.productId} className="products my-5">
-          <img src={image} alt={title} width="40px" height="40px" />
-          <p> {title} </p>
-          <p> x{product.quantity} </p>
-          <p> ${price} </p>
-        </div>
-      );
-    })
-  ) : (
-    <p> Something went wrong, please try again later </p>
-  );
-  return (
-    <Wrapper className="mx-5">
-      <div className="content-container">
-        <div className="payment-summary my-5">
-          <h3> Summary </h3>
-          {summary}
-          <hr />
-          <p>Total</p>
-          <h3> ${cartSummary.subtotal}</h3>
-        </div>
-        <PaymentForm
-          handlePayment={() => handlePayment()}
-          total={cartSummary.subtotal}
-        />
-        <Button onClick={stripePay}> Pay </Button>
-      </div>
-    </Wrapper>
-  );
+  return <Wrapper></Wrapper>;
 }
 
 const Wrapper = styled.section`
-  .products {
-    display: flex;
-    justify-content: space-around;
-  }
-  .payment-summary {
-    min-width: 40vw;
-    padding: 20px;
-    max-height: 80vh;
-    overflow-y: auto;
-  }
-  .content-container {
-    display: flex;
-    justify-content: space-around;
-  }
-  @media screen and (max-width: 1200px) {
-    .content-container {
-      flex-direction: column;
-    }
+  height: 100vh;
+  iframe {
+    width: 100vw;
+    height: 100vh;
   }
 `;
